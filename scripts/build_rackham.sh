1c169cbb26cbd2a37d6af6a61b8ace33f5f27f8b
richelbilderbeek <richel@richelbilderbeek.nl>
2023-06-16 09:24:49 +0200

    Added script for Rackham

---
+++ scripts/build_rackham.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 10:00
+#SBATCH -J build_rackham.log
+#SBATCH --qos=short
+#
+# Build the project on Rackham.
+#
+# Usage:
+#
+#   ./scripts/build_rackham.sh
+#
+#  Using sbatch, when being Richel:
+#
+#   sbatch -A uppmax2023-2-25 -M snowy scripts/build_rackham.sh
+#
+if [[ ! -z "${CLUSTER}" ]]; then
+  echo "Working on a cluster"
+else
+  echo "Not working on a cluster, stopping"
+  exit 42
+fi
+
+module load gcc/13.1.0 
+module load cmake/3.22.2 
+module load intel/20.4
+
+# Create the folder, even if it already exists
+mkdir build
+cd build
+rm -f CMakeCache.txt
+cmake ..
+cmake --build . --config=Release --target all -j 10
+cd ..
+
